2b021f08e722936a3daa055cde741511126580b6
Soyeon-Cha <89539031+Soyeon-Cha@users.noreply.github.com>
2022-10-12 00:14:39 +0900

    Create Exercise02_2.c

---
+++ Exercise02_2.c
@@ -0,0 +1,30 @@
+#include "msp.h"s
+
+
+/**
+ * main.c
+ */
+void main(void)
+{
+	WDT_A->CTL = WDT_A_CTL_PW | WDT_A_CTL_HOLD;		// stop watchdog timer
+	P1->DIR |= BIT0;
+
+	int i;
+	int j=100000;
+
+	while(1)
+	{
+	    P1->OUT=BIT0;
+	    for(i=1;i<j;i++)
+			;
+	    P1->OUT=0;
+	    for(i=1;i<j;i++)
+			;
+
+	    j = j - 2500;
+
+	    if(j<=3000){
+	        j = 3000;
+	    }
+	 }
+}
